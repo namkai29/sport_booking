cb9190bdc1e37ba048ce887f1932bca5019c4f8e
Le Van Dung <dunglv270404@gmail.com>
2026-04-14 23:24:24 +0700

    mthmth1

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/san", sanRoutes); //san
 app.use("/api/gia-san", giaRoutes); //gia san
 app.use("/api/lich-san", lichRoutes); //lịch snâ
-👉 Trang mặc định (login)
+//👉 Trang mặc định (login)
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
