3cff83a37770f5ff1cf5592702585cd31c5a1fe5
Le Van Dung <dunglv270404@gmail.com>
2026-04-17 15:06:39 +0700

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "15122004",
-    database: "sport_booking",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    charset: 'utf8mb4'
+  host: "localhost",
+  user: "root",
+  password: "27042004",
+  database: "sport_booking",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  charset: "utf8mb4",
 });
 
 console.log("MySQL Connected");
